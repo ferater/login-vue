b31dcafcdacb23bf51bb417bd19ca830790ddd81
Ferhat ER <lersmooth@gmail.com>
2020-05-12 16:59:30 +0300

    try catch yapılacak

--- src/api-services/crsf.js
+++ src/api-services/crsf.js
@@ -1,13 +1,23 @@
 import axios from 'axios';
+import { Cookies } from 'quasar';
 
 export default {
   /**
    * Get Crsf cookie from Laravel
    */
   getCookie() {
+    const token = Cookies.get('XSRF-TOKEN');
+    if (token) {
+      return new Promise((resolve) => {
+        resolve(token);
+        console.log('getCookie, (Actions, Then) : Csrf Çerez zaten var');
+      });
+    }
     return axios({
       method: 'get',
       url: '/sanctum/csrf-cookie',
+    }).then(() => {
+      console.log('getCookie, (Actions, Then) : Csrf Çerez alındı');
     });
   },
 };
--- src/store/module-auth/actions.js
+++ src/store/module-auth/actions.js
@@ -1,5 +1,5 @@
-import crsf from '../../api-services/crsf';
 import auth from '../../api-services/auth';
+import crsf from '../../api-services/crsf';
 
 /**
  * Login or Register
@@ -7,17 +7,23 @@ import auth from '../../api-services/auth';
  * @param {object} resource
  */
 export async function loginOrRegister(context, resource) {
-  await crsf.getCookie();
-  console.info('getCookie, (Actions, Then) : Çerez Alındı');
-  return auth.loginOrRegister(resource)
-    .then((response) => {
-      console.log('loginOrRegister, (Actions, Then) :', response);
-      this.$router.push({ name: 'home' });
+  await crsf.getCookie()
+    .then(() => {
+      auth.loginOrRegister(resource)
+        .then((response) => {
+          console.log('loginOrRegister, (Actions, Then) :', response);
+          this.$router.push({ name: 'home' });
+        })
+        .catch((error) => {
+          if (error.response.status === 422) {
+            context.commit('setFormErrors', error.response.data.errors);
+            console.log('loginOrRegister, (Actions, Catch) :', error.response);
+          }
+        });
     })
     .catch((error) => {
-      if (error.response.status === 422) {
-        context.commit('setFormErrors', error.response.data.errors);
-        console.info('loginOrRegister, (Actions, Catch) :', error.response);
+      if (!error.response) {
+        console.info('getCookie, (Actions, Catch) : Network Hatası!');
       }
     });
 }
