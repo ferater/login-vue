05a12cc33d09eb15269f576deba21c9866c05f88
Ferhat ER <lersmooth@gmail.com>
2020-06-05 16:49:16 +0300

    options

--- src/layouts/components/sidebar/index.vue
+++ src/layouts/components/sidebar/index.vue
@@ -1,16 +1,73 @@
 <template>
-  <div class="sidenav" v-if="$q.screen.gt.sm">
-    <ul class="list-unstyled">
-      <side-bar-item v-for="link in routes" :key="link.name" v-bind="link" />
-    </ul>
-    <div class="sidenav-bottom">
-      <q-btn flat icon="las la-cog" size="10px" @click="options = !options" />
+  <div>
+    <div class="sidenav" v-if="$q.screen.gt.sm">
+      <ul class="list-unstyled">
+        <side-bar-item v-for="link in routes" :key="link.name" v-bind="link" />
+      </ul>
+      <div class="sidenav-bottom">
+        <q-btn flat icon="las la-cog" size="10px" @click="options = !options" />
+      </div>
     </div>
-    <!-- <div class="options-container">
-      <div class="options" :class="{ open: options }">
-      sd
+    <div
+      class="options-container"
+      :class="{ open: options }"
+      @click="options = !options"
+    >
+      <div class="options scroll" :class="{ open: options }" @click.stop>
+        <div class="q-pa-md" style="max-width: 350px">
+          <q-list>
+            <q-expansion-item
+              dense
+              dense-toggle
+              expand-separator
+              icon="perm_identity"
+              label="Account settings"
+            >
+              <div>
+                <div class="q-gutter-sm">
+                  <q-checkbox v-model="teal" label="Teal" />
+                </div>
+              </div>
+            </q-expansion-item>
+
+            <q-expansion-item
+              dense
+              dense-toggle
+              expand-separator
+              icon="signal_wifi_off"
+              label="Wifi settings"
+            >
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quidem, eius reprehenderit eos corrupti commodi magni quaerat
+                  ex numquam, dolorum officiis modi facere maiores architecto
+                  suscipit iste eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+
+            <q-expansion-item
+              dense
+              dense-toggle
+              expand-separator
+              icon="drafts"
+              label="Drafts"
+              header-class="text-purple"
+            >
+              <q-card>
+                <q-card-section>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quidem, eius reprehenderit eos corrupti commodi magni quaerat
+                  ex numquam, dolorum officiis modi facere maiores architecto
+                  suscipit iste eveniet doloribus ullam aliquid.
+                </q-card-section>
+              </q-card>
+            </q-expansion-item>
+          </q-list>
+        </div>
+      </div>
     </div>
-    </div> -->
   </div>
 </template>
 
@@ -26,6 +83,7 @@ export default {
   data() {
     return {
       options: false,
+      teal: true,
     };
   },
   computed: {
